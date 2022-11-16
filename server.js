ac204fd7c348f3d31ac8c74fa57a1c6a0537139b
Gustavo Kazuo <73189635+kazuohagy@users.noreply.github.com>
2022-11-16 06:01:12 -0300

    test1

--- server.js
+++ server.js
@@ -37,6 +37,10 @@ app.get("/background/:city", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
